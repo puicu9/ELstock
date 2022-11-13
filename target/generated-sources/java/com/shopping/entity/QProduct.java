b2f1cf9c415b83b17bffeb9fe924d5340362fd5b
puicu9 <113329256+puicu9@users.noreply.github.com>
2022-11-13 17:38:12 +0900

    first_test

---
+++ target/generated-sources/java/com/shopping/entity/QProduct.java
@@ -0,0 +1,61 @@
+package com.shopping.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QProduct is a Querydsl query type for Product
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QProduct extends EntityPathBase<Product> {
+
+    private static final long serialVersionUID = 1654253981L;
+
+    public static final QProduct product = new QProduct("product");
+
+    public final QBaseEntity _super = new QBaseEntity(this);
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    public final StringPath description = createString("description");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final StringPath modifiedBy = _super.modifiedBy;
+
+    public final StringPath name = createString("name");
+
+    public final NumberPath<Integer> price = createNumber("price", Integer.class);
+
+    public final EnumPath<com.shopping.constant.ProductStatus> productStatus = createEnum("productStatus", com.shopping.constant.ProductStatus.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> regDate = _super.regDate;
+
+    public final NumberPath<Integer> stock = createNumber("stock", Integer.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updateDate = _super.updateDate;
+
+    public QProduct(String variable) {
+        super(Product.class, forVariable(variable));
+    }
+
+    public QProduct(Path<? extends Product> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QProduct(PathMetadata metadata) {
+        super(Product.class, metadata);
+    }
+
+}
+
---
+++ target/generated-sources/java/com/shopping/entity/QSaram.java
@@ -0,0 +1,43 @@
+package com.shopping.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QSaram is a Querydsl query type for Saram
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QSaram extends EntityPathBase<Saram> {
+
+    private static final long serialVersionUID = 665440062L;
+
+    public static final QSaram saram = new QSaram("saram");
+
+    public final StringPath address = createString("address");
+
+    public final StringPath id = createString("id");
+
+    public final StringPath name = createString("name");
+
+    public final NumberPath<Integer> salary = createNumber("salary", Integer.class);
+
+    public QSaram(String variable) {
+        super(Saram.class, forVariable(variable));
+    }
+
+    public QSaram(Path<? extends Saram> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QSaram(PathMetadata metadata) {
+        super(Saram.class, metadata);
+    }
+
+}
+
