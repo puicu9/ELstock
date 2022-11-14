2df8fd44e6de9e0e6e4be1cf782b1069a12a5915
puicu9 <113329256+puicu9@users.noreply.github.com>
2022-11-14 15:19:49 +0900

    DeleteAll

--- src/main/java/com/elstock/exception/OutOfStockException.java
+++
@@ -1,7 +0,0 @@
-package com.elstock.exception;
-
-public class OutOfStockException extends RuntimeException{
-    public OutOfStockException(String message) {
-        super(message) ;
-    }
-}
--- src/main/java/com/elstock/order/repository/OrderProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elstock.order.repository;
-
-import com.elstock.entity.OrderProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
-}
