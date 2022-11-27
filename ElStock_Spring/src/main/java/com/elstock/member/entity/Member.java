1103ae86e5c631a98af4d16d2072ed534e0da386
puicu9 <puicu9@gmail.com>
2022-11-27 19:36:01 +0900

    1127 reupdate

--- ElStock_Spring/src/main/java/com/elstock/member/entity/Member.java
+++ ElStock_Spring/src/main/java/com/elstock/member/entity/Member.java
@@ -19,30 +19,28 @@ public class Member {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id ;
+    private Long id;
 
-    private String name ;
+    private String name;
 
     @Column(unique = true)
-    private String email ;
-    private String password ;
-    private String address ;
-    private String nickname ;
+    private String email;
+    private String password;
+    private String address;
+    private String nickname;
 
     @Enumerated(EnumType.STRING)
-    private Role role ; // 일반 사용자, 관리자 모드 구분
+    private Role role; // 일반 사용자, 관리자 모드 구분
 
-    private LocalDateTime regDate ;
-    private int f_balance ;
+    private LocalDateTime regDate;
+    private int f_balance;
 
 
     // 폼 화면에서 넘어오는 dto 객체를 이용하여 해당 회원에 대한 비번번호의 암호화를 처리해주는 메소드입니다.
-<<<<<<< HEAD
-    public static Member createMember(MemberNewDto dto, PasswordEncoder passEncoder){
-=======
-    public static Member createMember(MemberFormDto dto, PasswordEncoder passEncoder) throws JSONException, IOException {
->>>>>>> main
-        Member member = new Member() ;
+
+
+    public static Member createMember(MemberNewDto dto, PasswordEncoder passEncoder) throws JSONException, IOException {
+        Member member = new Member();
 
         member.setName(dto.getName());
         member.setAddress(dto.getAddress());
@@ -50,15 +48,15 @@ public class Member {
         member.setRole(Role.USER); // 차후 관리자와 구분이 필요할 듯 ...
         // member.setId();
 
-        GetNickname getNickname = new GetNickname() ;
+        GetNickname getNickname = new GetNickname();
         String nickname = getNickname.Nickname();
         member.setNickname(nickname);
 
 
-        String password = passEncoder.encode(dto.getPassword()) ; // 비번 암호화
+        String password = passEncoder.encode(dto.getPassword()); // 비번 암호화
         member.setPassword(password);
         member.setF_balance(100000000);
 
-        return member ;
+        return member;
     }
 }
