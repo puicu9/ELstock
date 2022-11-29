9f72db4e79a989d28f67e88ef0ec6ca66dec6294
puicu9 <puicu9@gmail.com>
2022-11-29 17:31:37 +0900

    Revert "1129 개발환경 구축"
    This reverts commit 55524926f44c657897f3f80ae1ac10e967f1be99.

--- ElStock_Spring/src/main/java/com/elstock/exception/OutOfStockException.java
+++
@@ -1,7 +0,0 @@
-package com.elstock.exception;
-// OutOfStockException01
-public class OutOfStockException extends RuntimeException{
-    public OutOfStockException(String message) {
-        super(message) ;
-    }
-}
