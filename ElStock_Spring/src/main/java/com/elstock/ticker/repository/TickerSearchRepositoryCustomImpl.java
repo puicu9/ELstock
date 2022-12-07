f9f9dbf1c944b1f60e0c2adc66c5b19a265fc6ed
puicu9 <puicu9@gmail.com>
2022-12-07 11:52:58 +0900

    1207 정리

--- ElStock_Spring/src/main/java/com/elstock/ticker/repository/TickerSearchRepositoryCustomImpl.java
+++ ElStock_Spring/src/main/java/com/elstock/ticker/repository/TickerSearchRepositoryCustomImpl.java
@@ -1,16 +1,17 @@
 package com.elstock.ticker.repository;
 
 import com.elstock.ticker.dto.TickerSearchDto;
+import com.elstock.ticker.entity.Market;
 import com.elstock.ticker.entity.QMarket;
-import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class TickerSearchRepositoryCustomImpl implements TickerSearchRepositoryCustom {
@@ -46,18 +47,33 @@ public class TickerSearchRepositoryCustomImpl implements TickerSearchRepositoryC
 
 
     @Override
-    public Page<Tuple> getTickerPage(TickerSearchDto dto, Pageable pageable) {
-        List<Tuple> content = this.queryFactory
-                .select(QMarket.market.ticker_code,
-                        QMarket.market.ticker_name
+    public Page<Market> getTickerPage(TickerSearchDto dto, Pageable pageable) {
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        String dateTimeString = dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd 00:00:00"));
+//        LocalDateTime today = LocalDateTime.parse(dateTime, dateTimeString);
+
+        List<Market> content = this.queryFactory
+                .selectFrom(QMarket.market)
+                .where(searchQueryCondition(
+                        dto.getSearchQuery()),
+                    QMarket.market.date.eq(LocalDateTime.parse(dateTimeString))
                 )
-                .from(QMarket.market)
-                .where(searchQueryCondition(dto.getSearchQuery()))
                 .orderBy(QMarket.market.ticker_name.asc())
-                .groupBy(QMarket.market.ticker_name, QMarket.market.ticker_code)
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
+//
+//                .select(QMarket.market.ticker_code,
+//                        QMarket.market.ticker_name
+//                )
+//                .from(QMarket.market)
+//                .where(searchQueryCondition(dto.getSearchQuery()))
+//                .orderBy(QMarket.market.ticker_name.asc())
+//                .groupBy(QMarket.market.ticker_name, QMarket.market.ticker_code)
+//                .offset(pageable.getOffset())
+//                .limit(pageable.getPageSize())
+//                .fetch();
 
 
         return new PageImpl<>(content, pageable, content.size());
