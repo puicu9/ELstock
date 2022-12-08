601456863304484f1c8da01975cc16679aee7c47
puicu9 <puicu9@gmail.com>
2022-12-08 23:16:02 +0900

    1209 상완

--- ElStock_Spring/src/main/java/com/elstock/ticker/repository/TickerSearchRepositoryCustomImpl.java
+++ ElStock_Spring/src/main/java/com/elstock/ticker/repository/TickerSearchRepositoryCustomImpl.java
@@ -2,13 +2,22 @@ package com.elstock.ticker.repository;
 
 import com.elstock.ticker.dto.TickerSearchDto;
 import com.elstock.ticker.entity.QMarket;
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 public class TickerSearchRepositoryCustomImpl implements TickerSearchRepositoryCustom {
     private JPAQueryFactory queryFactory ;
@@ -28,11 +37,6 @@ public class TickerSearchRepositoryCustomImpl implements TickerSearchRepositoryC
         }
 
         
-        // 실행하기 위해 만듦 ,, 삭젷애ㅑ함
-    @Override
-    public Page<Tuple> getTickerPage(TickerSearchDto dto, Pageable pageable) {
-        return null;
-    }
 
 //        if(StringUtils.equals("ticker_name", searchQuery)){
 //            System.out.println("aaaaa");
@@ -82,24 +86,37 @@ public class TickerSearchRepositoryCustomImpl implements TickerSearchRepositoryC
 //        return new PageImpl<>(content, pageable, content.size());
 //    }
 
-//    @Override
-//    public Page<Tuple> getTickerPage(TickerSearchDto dto, Pageable pageable) {
-//        QMarket market = QMarket.market;
-//
-//        QueryResults<Tuple> result = this.queryFactory
-//                .select(market.ticker_code, market.ticker_name)
-//                .from(market)
-//                .where(searchQueryCondition(dto.getSearchQuery()))
-//                .orderBy(market.ticker_name.asc())
-//                .groupBy(market.ticker_name, market.ticker_code)
-//                .offset(pageable.getOffset())
-//                .limit(pageable.getPageSize())
-//                .fetchResults();
-//        List<Tuple> content = result.getResults();
-//
-//        // 결과 집합의 총 개수 반환
-//        long total = result.getTotal();
-//
-//        return new PageImpl<>(content, pageable, total);
-//    }
+    private BooleanExpression dateRange(){
+        // 사용자가 지정한 특정 기간 내의 데이터만 조회해주는 메소드입니다.
+        LocalDateTime dateTime = LocalDateTime.now() ;
+        
+        // after, before 등이 있습니다.
+        //return QMarket.date.after(dateTime) ;
+        BooleanExpression after = QMarket.market.date.between(dateTime,dateTime);
+        return after;
+
+    }
+
+    @Override
+    public Page<Tuple> getTickerPage(TickerSearchDto dto, Pageable pageable) {
+        QMarket market = QMarket.market;
+
+        QueryResults<Tuple> result = this.queryFactory
+                .select(market.ticker_code, market.ticker_name)
+                .from(market)
+                .where(searchQueryCondition(dto.getSearchQuery()))
+                .orderBy(market.ticker_name.asc())
+                .groupBy(market.ticker_name, market.ticker_code)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+        List<Tuple> content = result.getResults();
+
+        // 결과 집합의 총 개수 반환
+        long total = result.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+
 }
