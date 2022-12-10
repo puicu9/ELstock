05b3e0c7a208be19b556f746bef19ca2523d86a1
puicu9 <puicu9@gmail.com>
2022-12-10 19:05:29 +0900

    재환 업로드

--- ElStock_Spring/src/main/resources/static/chart/js/chart.js
+++ ElStock_Spring/src/main/resources/static/chart/js/chart.js
@@ -215,8 +215,8 @@ function drawChart(prices) {
             clearTimeout(resizeTimer)
             resizeTimer = setTimeout(function() {
 
-                var xmin = new Date(xDateScale(Math.floor(xScaleZ.domain()[0])))
-                xmax = new Date(xDateScale(Math.floor(xScaleZ.domain()[1])))
+                var xmin = new Date(xDateScale(Math.floor(xScaleZ.domain()[0])));
+                xmax = new Date(xDateScale(Math.floor(xScaleZ.domain()[1])));
 
                 filtered = _.filter(prices, d => {
                     const testMin = xmax.getTime();
