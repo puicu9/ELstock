c77d668b3c773534ab7bfbebc73d94de8d462b4f
puicu9 <puicu9@gmail.com>
2022-12-10 17:49:21 +0900

    초기화

--- ElStock_Spring/src/main/java/com/elstock/chart/repository/TeslaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.elstock.chart.repository;
-
-import com.elstock.chart.entity.Tesla;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeslaRepository extends JpaRepository<Tesla, Long>{
-
-}
--- ElStock_Spring/src/main/java/com/elstock/news/repository/NewsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.elstock.news.repository;
-
-import com.elstock.news.entity.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<News, Long>{
-
-}
